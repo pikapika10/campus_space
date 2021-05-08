929218b3e0a55d8e8ba2db8075fbf3e20d9d49d6
Gyanendra Kumar Knojiya <36006908+gyanendraknojiya@users.noreply.github.com>
2021-05-09 00:29:02 +0530

    changes in ui for homepage

--- client/src/Pages/FacultyStudentLoginPags.jsx
+++ client/src/Pages/FacultyStudentLoginPags.jsx
@@ -4,6 +4,10 @@ import { useHistory, Link } from "react-router-dom";
 import { facultyLogin } from "../redux/action/facultyAction";
 import { studentLogin } from "../redux/action/studentAction";
 import classnames from "classnames";
+import StudentLoginLogo from "../Style/Images/student-login.svg";
+import Logo from "../Style/Images/Logo.png"
+
+import { Button, Form } from "semantic-ui-react";
 
 import "../Style/facultyStudentLogin.css";
 
@@ -19,7 +23,7 @@ const FacultyStudentLoginPags = () => {
   const [isFacultyLoading, setIsFacultyLoading] = useState(false);
   const [isStudentLoading, setIsStudentLoading] = useState(false);
 
-  const [showFacultyLogin, setShowFacultyLogin] =  useState(false)
+  const [showFacultyLogin, setShowFacultyLogin] = useState(false);
 
   const history = useHistory();
 
@@ -48,8 +52,6 @@ const FacultyStudentLoginPags = () => {
 
   const facultyFormHandler = (e) => {
     e.preventDefault();
-    let registrationNumber;
-    let password;
     setIsFacultyLoading(true);
     dispatch(
       facultyLogin({
@@ -87,144 +89,167 @@ const FacultyStudentLoginPags = () => {
   }, [store.errorHelper, store.student.isAuthenticated]);
 
   return (
-    <div className="container-fluid">
-      <div className="row" id="trail" style={{height:"100vh"}} >
-       {showFacultyLogin? 
-                  <div
-              className="col-md-4 m-auto border shadow"
-              style={{
-                backgroundColor: "white",
-                borderRadius: "1.2rem",
-                padding: "1rem 1rem 0rem 1rem",
-              }}
-            >
-              <div>
-                <h3 className="text-center "><i class="fas fa-user"></i> FACULTY</h3>
-                <form noValidate onSubmit={facultyFormHandler}>
-                  <div className="form-group">
-                    <label htmlFor="facRegId">Registration Number</label>
-                    <input
-                      onChange={(e) => setFacultyRegNum(e.target.value)}
-                      type="text"
-                      value={facultyRegNum}
-                      className={classnames("form-control", {
-                        "is-invalid": errors.registrationNumber,
-                      })}
-                      id="facRegId"
-                    />
-                    {errors.registrationNumber && (
-                      <div className="invalid-feedback">
-                        {errors.registrationNumber}
-                      </div>
-                    )}
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="passwordFacId">Password</label>
-                    <input
-                      onChange={(e) => setFacultyPassword(e.target.value)}
-                      value={facultyPassword}
-                      className={classnames("form-control", {
-                        "is-invalid": errors.password,
-                      })}
-                      type="password"
-                      id="passwordFacId"
-                    />
-                    {errors.password && (
-                      <div className="invalid-feedback">{errors.password}</div>
-                    )}
-                  </div>
-                  <div class="row justify-content-center">
-                    <div class="col-md-1">
-                      {isFacultyLoading && (
-                        <div class="spinner-border text-primary" role="status">
-                          <span class="sr-only">Loading...</span>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-
-                  {!isFacultyLoading && (
-                    <button type="submit" className="btn btn-info btn-block">
-                      <i class="fas fa-sign-in-alt"></i> Login
-                    </button>
-                  )}
-                </form>
-              
-                <div className="bg-success text-white py-2 text-center my-2 cp rounded-pill" onClick={()=> setShowFacultyLogin(false)} >Go to faculty login</div>
-  
+      <div className="row mx-0" id="trail" style={{ height: "100vh" }}>
+        <span style={{
+          position:"absolute",
+          top:"5px",
+          left:"15px"
+        }} ><img src={Logo} alt="Logo" height="35px" /></span>
+        <div className="col-6 m-auto text-center">
+          <img src={StudentLoginLogo} alt="StudentLoginLogo" height="400px" />
+        </div>
+        <div
+          className="col-md-5 px-5 m-auto bg-light rounded pt-5"
+          style={{
+            height: "90vh",
+            overflowY: "auto",
+          }}
+        >
+          {showFacultyLogin ? (
+            <div>
+              <h3 className=" jumbotron text-secondary">
+                <h2 className="text-dark">Faculty Login</h2>
+                <p>
+                  <small>
+                    Please login using your registraion number and password...
+                  </small>
+                </p>
+              </h3>
+              <Form noValidate onSubmit={facultyFormHandler}>
+                <Form.Field>
+                  <Form.Input
+                    error={
+                      errors.registrationNumber && {
+                        content: errors.registrationNumber,
+                        pointing: "below",
+                      }
+                    }
+                    icon="users"
+                    iconPosition="left"
+                    fluid
+                    label="Registration Number"
+                    placeholder="Registration Number"
+                    id="form-input-Registration Number"
+                    onChange={(e) => {
+                      setFacultyRegNum(e.target.value);
+                      setErrors({});
+                    }}
+                    type="text"
+                    value={facultyRegNum}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <Form.Input
+                    error={
+                      errors.password && {
+                        content: errors.password,
+                        pointing: "below",
+                      }
+                    }
+                    fluid
+                    icon="lock"
+                    iconPosition="left"
+                    label="Password"
+                    id="form-input-Registration Number"
+                    onChange={(e) => {
+                      setFacultyPassword(e.target.value);
+                      setErrors({});
+                    }}
+                    placeholder="Password"
+                    type="password"
+                    value={facultyPassword}
+                  />
+                </Form.Field>
+                <div className="text-right">
+                  <Button type="submit" primary>
+                    <i class="fas fa-sign-in-alt mr-1"></i>Login
+                  </Button>
+                </div>
+              </Form>
+              <div className=" text-center my-2 ">
+                <span
+                  className="cp text-info"
+                  onClick={() => setShowFacultyLogin(false)}
+                >
+                  Go to student login
+                </span>
               </div>
-            </div>:
-         <div
-         className="col-md-4 m-auto border shadow"
-         style={{
-           backgroundColor: "white",
-           borderRadius: "1.2rem",
-           padding: "1rem 1rem 0rem 1rem",
-         }}
-       >
-         <div>
-           <h3 className="text-center"><i class="fas fa-user"></i> STUDENT</h3>
-           <form noValidate onSubmit={studentFormHandler}>
-             <div className="form-group">
-               <label htmlFor="studentId">Registration Number</label>
-               <input
-               placeholder="Enter your registraion number"
-                 onChange={(e) => setStudentRegNum(e.target.value)}
-                 type="text"
-                 value={studentRegNum}
-                 className={classnames("form-control", {
-                   "is-invalid": errorsHelper.registrationNumber,
-                 })}
-                 id="studentId"
-               />
-               {errorsHelper.registrationNumber && (
-                 <div className="invalid-feedback">
-                   {errorsHelper.registrationNumber}
-                 </div>
-               )}
-             </div>
-             <div className="form-group">
-               <label htmlFor="passwordId">Password</label>
-               <input
-                placeholder="Enter your password"
-                 onChange={(e) => setStudentPassword(e.target.value)}
-                 value={studentPassword}
-                 className={classnames("form-control", {
-                   "is-invalid": errorsHelper.password,
-                 })}
-                 type="password"
-                 id="passwordId"
-               />
-               {errorsHelper.password && (
-                 <div className="invalid-feedback">
-                   {errorsHelper.password}
-                 </div>
-               )}
-             </div>
-             <div class="row justify-content-center">
-               <div class="col-md-1">
-                 {isStudentLoading && (
-                   <div class="spinner-border text-primary" role="status">
-                     <span class="sr-only">Loading...</span>
-                   </div>
-                 )}
-               </div>
-             </div>
-             {!isStudentLoading && (
-               <button type="submit" className="btn btn-info btn-block ">
-                 <i class="fas fa-sign-in-alt"></i> Login
-               </button>
-             )}
-           </form>
-           
-           <div className="bg-primary py-2 text-center my-2  cp rounded-pill text-white" onClick={()=> setShowFacultyLogin(true)} >Go to faculty login</div>
-         </div>
-       </div>
+            </div>
+          ) : (
+            <div>
+              <h3 className=" jumbotron text-secondary">
+                <h2 className="text-dark">Student Login</h2>
+                <p>
+                  <small>
+                    Please login using your registraion number and password...
+                  </small>
+                </p>
+              </h3>
+              <Form noValidate onSubmit={studentFormHandler}>
+                <Form.Field>
+                  <Form.Input
+                    error={
+                      errorsHelper.registrationNumber && {
+                        content: errorsHelper.registrationNumber,
+                        pointing: "below",
+                      }
+                    }
+                    icon="users"
+                    iconPosition="left"
+                    fluid
+                    label="Registration Number"
+                    placeholder="Registration Number"
+                    id="form-input-Registration Number"
+                    onChange={(e) => {
+                      setStudentRegNum(e.target.value);
+                      setErrorsHelper({});
+                    }}
+                    type="text"
+                    value={studentRegNum}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <Form.Input
+                    error={
+                      errorsHelper.password && {
+                        content: errorsHelper.password,
+                        pointing: "below",
+                      }
+                    }
+                    fluid
+                    icon="lock"
+                    iconPosition="left"
+                    label="Password"
+                    id="form-input-Registration Number"
+                    onChange={(e) => {
+                      setStudentPassword(e.target.value);
+                      setErrorsHelper({});
+                    }}
+                    placeholder="Password"
+                    type="password"
+                    value={studentPassword}
+                  />
+                </Form.Field>
+                <div className="text-right">
+                  <Button type="submit" primary>
+                    <i class="fas fa-sign-in-alt mr-1"></i>Login
+                  </Button>
+                </div>
+              </Form>
 
- } 
-      
+              <div className=" text-center my-2 ">
+                <span
+                  className="cp text-info"
+                  onClick={() => setShowFacultyLogin(true)}
+                >
+                  Go to faculty login
+                </span>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    
   );
 };
 
